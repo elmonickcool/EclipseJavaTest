7ff4cbff0e54705c065eeee04032562ecdac396c
Elmo Nickol <41835231+elmonickcool@users.noreply.github.com>
2023-11-17 23:40:26 +0800

    added grades status

--- TestJavaProjectGit/src/GradesAverage.java
+++ TestJavaProjectGit/src/GradesAverage.java
@@ -12,21 +12,34 @@ public class GradesAverage {
 		System.out.print("English: ");
 		int eng = s.nextInt();
 		
-		System.out.println("Math: ");
+		System.out.print("Math: ");
 		int mat = s.nextInt();
 		
-		System.out.println("Science: ");
+		System.out.print("Science: ");
 		int sci = s.nextInt();
 		
-		System.out.println("Computer: ");
+		System.out.print("Computer: ");
 		int com = s.nextInt();
 		
 		int total = eng+mat+sci+com;
 		float ave = total/4.00f;
 		
-		
-		
 		System.out.println("Average: "+ave);
+		
+		if(ave>100) {
+			System.out.println("Invalid Grade");
+		}else if(ave>=98){
+			System.out.println("With Highest Honor");
+		}else if(ave>=95){
+			System.out.println("With High Honor");
+		}else if(ave>=90){
+			System.out.println("With Honor");
+		}else if(ave>=75){
+			System.out.println("Passed!");
+		}else {
+			System.out.println("Failed");
+		}
+		
 		s.close();
 
 	}
