dfb54fbdaa39314315b5df297d8e3e40a656dd1e
Elmo Nickol <41835231+elmonickcool@users.noreply.github.com>
2023-11-17 23:28:04 +0800

    added subject score and average

--- TestJavaProjectGit/src/GradesAverage.java
+++ TestJavaProjectGit/src/GradesAverage.java
@@ -1,8 +1,33 @@
 
+import java.util.Scanner;
+
 public class GradesAverage {
+	
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner s = new Scanner(System.in);
+		
+		System.out.println("Grade Average");
+		
+		System.out.print("English: ");
+		int eng = s.nextInt();
+		
+		System.out.println("Math: ");
+		int mat = s.nextInt();
+		
+		System.out.println("Science: ");
+		int sci = s.nextInt();
+		
+		System.out.println("Computer: ");
+		int com = s.nextInt();
+		
+		int total = eng+mat+sci+com;
+		float ave = total/4.00f;
+		
+		
+		
+		System.out.println("Average: "+ave);
+		s.close();
 
 	}
 
