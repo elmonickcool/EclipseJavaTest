08e5ecba1d86af7b3709fb70e8762ae1da08db28
Elmo Nickol <41835231+elmonickcool@users.noreply.github.com>
2023-11-18 18:51:54 +0800

    added day

--- TestJavaProjectGit/src/DateFormatter.java
+++ TestJavaProjectGit/src/DateFormatter.java
@@ -11,13 +11,57 @@ public class DateFormatter {
 		System.out.print("Month: ");
 		int month = s.nextInt();
 		
+		System.out.println("Day: ");
+		int day = s.nextInt();
+		
+		System.out.println("Year:");
+		int year = s.nextInt();
+		
 		switch(month) {
 		
 		case 1:
-			System.out.println("January");
+			System.out.print("January "+ day);
 			break;
 		case 2:
-			System.out.println("February");
+			System.out.print("February "+day);
+			break;
+		case 3:
+			System.out.print("March "+day);
+			break;
+		case 4:
+			System.out.print("April "+ day);
+			break;
+		case 5:
+			System.out.print("May ");
+			System.out.print(day);
+			break;
+		case 6:
+			System.out.print("June ");
+			System.out.print(day);
+			break;
+		case 7:
+			System.out.print("July ");
+			System.out.print(day);
+			break;
+		case 8:
+			System.out.print("August ");
+			System.out.print(day);
+			break;
+		case 9:
+			System.out.print("September ");
+			System.out.print(day);
+			break;
+		case 10:
+			System.out.print("October ");
+			System.out.print(day);
+			break;
+		case 11:
+			System.out.print("November ");
+			System.out.print(day);
+			break;
+		case 12:
+			System.out.print("December ");
+			System.out.print(day);
 			break;
 		default:
 			System.out.println("Invalid Month");
