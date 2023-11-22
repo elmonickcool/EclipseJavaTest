282b77bf29064c6e8286c7c050b1af28e489ae4e
Elmo Nickol <41835231+elmonickcool@users.noreply.github.com>
2023-11-23 00:47:29 +0800

    While Loop

---
+++ TestJavaProjectGit/src/DoWhileLoop.java
@@ -0,0 +1,15 @@
+
+public class DoWhileLoop {
+
+	public static void main(String[] args) {
+		int i=0 ;
+		
+		do {
+			System.out.println(i);
+			i++;
+			break;
+		}while(i<5);
+
+	}
+
+}
---
+++ TestJavaProjectGit/src/WhileLoop.java
@@ -0,0 +1,30 @@
+
+import java.util.Scanner;
+
+public class WhileLoop {
+
+	public static void main(String[] args) {
+		
+		Scanner s = new Scanner(System.in);
+		
+		String[] names = {"Daniel","John","Jake","Elmo","Arvin","Kent","Kara","Jasmine","Dana","Mika"};
+		System.out.print("Search Names: ");
+		String search = s.nextLine();
+		
+		int  i =0;
+		
+		while(i<names.length) {
+			if(names[i].equalsIgnoreCase(search)) {
+				System.out.println("We found " + names[i]);
+				break;	
+			}
+			i++;
+			
+		}
+		s.close();
+		
+		
+
+	}
+
+}
